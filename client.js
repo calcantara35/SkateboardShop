bc836b959bf0389f5232de1efee026bd2d24dc05
Cesar Alcantara <cja5391@psu.edu>
2019-11-15 14:57:15 -0500

    fixed Table issue

--- client.js
+++ client.js
@@ -1,13 +1,15 @@
 // init xmlhttpreq
 let request = new XMLHttpRequest();
 
+let resData = null;
+
 // setting properties
 request.open("GET", "http://localhost:4500/reservations/all", true);
 
 // specific whats gonna happen once we get data back | This is a callback function, excutes once we have data
 request.onload = function() {
   // use this to refer the api
-  let resData = JSON.parse(this.response); // contains data from current object | Payload is this.response | parse turns it into a js object
+  resData = JSON.parse(this.response); // contains data from current object | Payload is this.response | parse turns it into a js object
   console.log(resData);
 
   if (request.status == 200) {
@@ -16,13 +18,19 @@ request.onload = function() {
 
     // looping through response data
     resData.forEach(reservation => {
-      // appending to output variable
-      output += `<tr><td>${reservation.name}</td>
-      <td>${reservation.date}</td>
-      <td>${reservation.time}</td>
-      <td>${reservation.hours}</td></tr>`;
+      const reslist = document.getElementById("reservation-data");
+      // Create tr element
+      const row = document.createElement("tr");
+      // insert cols
+      row.innerHTML = `
+          <td>${reservation.name}</td>
+          <td>${reservation.date}</td>
+          <td>${reservation.time}</td>
+          <td>${reservation.hours}</td>
+          <td><a href='#' class='delete'>X</a></td>`;
+      reslist.appendChild(row);
     });
-    document.querySelector("#allResData").innerHTML = output;
+    // document.querySelector("#username").innerHTML = output;
     console.log("Success!");
   } else {
     console.log("An error occured");
