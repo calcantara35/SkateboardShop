9b8f01a8b9d242bac96c8f39388dd0917116e4ca
Cesar Alcantara <cja5391@psu.edu>
2019-11-20 10:40:19 -0500

    tested endpoints with curl added comments

--- app.js
+++ app.js
@@ -26,7 +26,9 @@ let resFile = "Reservation.json";
 
 // home page
 server.get("/", (req, res) => {
-  res.send("<h1>Welcome to the skateboard shop. Make some reservations today!</h1");
+  res.send(
+    "<h1>Welcome to the skateboard shop. Make some reservations today!</h1"
+  );
 });
 
 // add new username
@@ -45,7 +47,9 @@ server.post("/register/new/:username", (req, res) => {
       if (err) throw err;
       console.log("Success");
     });
-    res.send(`The File ${usersFile} has been created and the user ${userName} has been added!`);
+    res.send(
+      `The File ${usersFile} has been created and the user ${userName} has been added!`
+    );
   } else {
     // if it exists, read Users.json file
     fs.readFile(usersFile, (err, data) => {
@@ -73,57 +77,62 @@ server.post("/register/new/:username", (req, res) => {
 });
 
 // Create reservation
-server.post("/reservation/new/user/:username/date/:startDate/time/:startTime/hours/:resHours", (req, res) => {
-  let userName = req.params.username;
-  let startDate = req.params.startDate;
-  let startTime = req.params.startTime;
-  let hours = req.params.resHours;
+server.post(
+  "/reservation/new/user/:username/date/:startDate/time/:startTime/hours/:resHours",
+  (req, res) => {
+    let userName = req.params.username;
+    let startDate = req.params.startDate;
+    let startTime = req.params.startTime;
+    let hours = req.params.resHours;
 
-  // checking to see if the file exists
-  if (!fs.existsSync(resFile)) {
-    // adding new user to empty object
-    resList.push({
-      name: userName,
-      date: startDate,
-      time: startTime,
-      hours
-    });
-
-    // creating the Reservation.json file
-    fs.writeFile(resFile, JSON.stringify(resList), err => {
-      if (err) throw err;
-      console.log("Success");
-    });
-    res.send(`New file has been created and the reservation for ${userName} has been added!`);
-  } else {
-    // if it exists, read Reservation.json file
-    fs.readFile(resFile, (err, data) => {
-      if (err) throw err;
-
-      // parsing through existing data and putting it in a variable and converting JSON object to JS object
-      let appendResList = JSON.parse(data);
-
-      // appending new user to existing file
-      appendResList.push({
+    // checking to see if the file exists
+    if (!fs.existsSync(resFile)) {
+      // adding new user to empty object
+      resList.push({
         name: userName,
         date: startDate,
         time: startTime,
         hours
       });
 
-      // converting js object back to JSON object
-      let appendResListInfo = JSON.stringify(appendResList);
-
-      // must be writefile or it will constantly create new objects
-      fs.writeFile(resFile, appendResListInfo, err => {
+      // creating the Reservation.json file
+      fs.writeFile(resFile, JSON.stringify(resList), err => {
         if (err) throw err;
-        console.log("Success!");
+        console.log("Success");
       });
-    });
-    res.send(`Reservation for ${userName} has been added to system.`);
+      res.send(
+        `New file has been created and the reservation for ${userName} has been added!`
+      );
+    } else {
+      // if it exists, read Reservation.json file
+      fs.readFile(resFile, (err, data) => {
+        if (err) throw err;
+
+        // parsing through existing data and putting it in a variable and converting JSON object to JS object
+        let appendResList = JSON.parse(data);
+
+        // appending new user to existing file
+        appendResList.push({
+          name: userName,
+          date: startDate,
+          time: startTime,
+          hours
+        });
+
+        // converting js object back to JSON object
+        let appendResListInfo = JSON.stringify(appendResList);
+
+        // must be writefile or it will constantly create new objects
+        fs.writeFile(resFile, appendResListInfo, err => {
+          if (err) throw err;
+          console.log("Success!");
+        });
+      });
+      res.send(`Reservation for ${userName} has been added to system.`);
+    }
+    // }
   }
-  // }
-});
+);
 
 // get reservation for all users
 server.get("/reservations/all", (req, res) => {
@@ -165,7 +174,9 @@ server.get("/reservation/user/:username", (req, res) => {
     let arrayList = JSON.stringify(resList);
     fs.writeFile(resFile, arrayList, err => {
       if (err) throw err;
-      console.log("User does not exist and there are no reservations at this time...");
+      console.log(
+        "User does not exist and there are no reservations at this time..."
+      );
     });
   } //else {
   fs.readFile(resFile, (err, data) => {
@@ -174,54 +185,59 @@ server.get("/reservation/user/:username", (req, res) => {
     let resFullList = JSON.parse(data);
     let stringUserResList = JSON.stringify(resFullList);
     console.log(stringUserResList);
+    // Returning entire file because we are going to do the forEach on client
     res.end(stringUserResList);
   });
   //}
 });
 
 // update reservation for a given user
-server.put("/reservation/update/user/:username/date/:startDate/time/:startTime/hours/:resHours", (req, res) => {
-  let userName = req.params.username;
-  let startDate = req.params.startDate;
-  let startTime = req.params.startTime;
-  let hours = req.params.resHours;
+server.put(
+  "/reservation/update/user/:username/date/:startDate/time/:startTime/hours/:resHours",
+  (req, res) => {
+    let userName = req.params.username;
+    let startDate = req.params.startDate;
+    let startTime = req.params.startTime;
+    let hours = req.params.resHours;
 
-  let userExists = false;
+    let userExists = false;
 
-  if (!fs.existsSync(resFile)) {
-    let arrayList = JSON.stringify(resList);
-    fs.writeFile(resFile, arrayList, err => {
+    if (!fs.existsSync(resFile)) {
+      let arrayList = JSON.stringify(resList);
+      fs.writeFile(resFile, arrayList, err => {
+        if (err) throw err;
+        res.end("There is no data for reservations.");
+      });
+    } //else {
+    fs.readFile(resFile, (err, data) => {
       if (err) throw err;
-      res.end("There is no data for reservations.");
-    });
-  } //else {
-  fs.readFile(resFile, (err, data) => {
-    if (err) throw err;
-    let fullResList = JSON.parse(data);
+      let fullResList = JSON.parse(data);
 
-    fullResList.forEach((reservation, index) => {
-      if (reservation.name.toString() === userName) {
-        fullResList[index].date = startDate;
-        fullResList[index].time = startTime;
-        fullResList[index].hours = hours;
+      fullResList.forEach((reservation, index) => {
+        if (reservation.name.toString() === userName) {
+          fullResList[index].date = startDate;
+          fullResList[index].time = startTime;
+          fullResList[index].hours = hours;
 
-        let convertResList = JSON.stringify(fullResList);
-        res.send(`Reservation: ${convertResList}`);
+          let convertResList = JSON.stringify(fullResList);
+          // sending entire list of reservations with updated user
+          res.send(`Reservation: ${convertResList}`);
 
-        userExists = true;
-
-        fs.writeFile(resFile, convertResList, err => {
-          if (err) throw err;
-          console.log("Updated File");
-        });
+          userExists = true;
+          // updated file, check reservation.json file
+          fs.writeFile(resFile, convertResList, err => {
+            if (err) throw err;
+            console.log("Updated File");
+          });
+        }
+      });
+      if (userExists === false) {
+        res.end("User does not exist.");
       }
     });
-    if (userExists === false) {
-      res.end("User does not exist.");
-    }
-  });
-  // }
-});
+    // }
+  }
+);
 
 // delete res file
 server.delete("/reservation/delete/user/:username/", (req, res) => {
